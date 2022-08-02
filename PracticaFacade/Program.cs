d08d4a32dd0471368591d4e1ecbc82971cf8fabd
Diego <Diego@DESKTOP-2K7JKQG>
2022-08-01 22:38:45 -0500

    Agregar archivos de proyecto.

---
+++ PracticaFacade/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace PracticaDecorador
+
+{
+
+    internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var OrdenRegular = new OrdenRegular();
+        Console.WriteLine(OrdenRegular.CalculoTotalPrecio());
+        Console.WriteLine();
+
+        var preOrder = new PreOrden();
+        Console.WriteLine(preOrder.CalculoTotalPrecio());
+        Console.WriteLine();
+        var ordenPremium = new OrdenPremium(preOrder);
+        Console.WriteLine(ordenPremium.CalculoTotalPrecio());
+        Console.WriteLine();
+    }
+}
+}
